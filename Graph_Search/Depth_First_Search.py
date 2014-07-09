88b978b1e3e7db55030529bee8fb5d8732455a23
Partha Sarathi Mukherjee <partha.s.m.2.010@gmail.com>
2014-07-10 00:26:15 +0530

    Update Depth_First_Search.py
    Introducing simple file-reading and graph as dictionary instead of list

--- Graph_Search/Depth_First_Search.py
+++ Graph_Search/Depth_First_Search.py
@@ -1,35 +1,33 @@
-def dfs(graph,seen,v):
+def dfs(seen,v):
     seen.append(v)
-    vc = [vc for vc in graph if vc[0]==v]
-    print vc[0]
-    vc = vc[0]
-    for v1 in vc[1]:
-        if v1 not in seen:
-            dfs(graph,seen,v1)
+    vc = Graph[v]
+    if len(vc)>0:
+        for v1 in vc:
+            if v1 not in seen:
+                dfs(seen,v1)
     
+import time
+start_time = time.time()
 
-#fi = open('kargerMinCut.txt')
-fi = open('test_case_dfs_3.txt')
-graph=[]
+
+fi = open('test_case.txt')
+Graph={}
 for line in fi:
-    string = line.strip()
-    tab_index = string.find('\t')
-    vertex = string[:tab_index]
-    string = string[tab_index+1:]
-    connected_to=[]
-    while tab_index!=-1:
-        connected_to.append(string[:tab_index])
-        string = string[tab_index+1:]
-        tab_index = string.find('\t')
-    connected_to.append(string)    
-    graph.append([vertex, connected_to])
+    li = line.split()
+    Graph[li[0]]=li[1:]
+
+#created a dictionary with 1st vertex of each row as key and the list containing the rest connected vertices
+#as value
+fi.close()
+    
 #print graph    
-#created a list with each row as an item in the list
-#the rows themselves are lists with the 1st element as the vertex
-#the 2nd element of the row is a list containing all the adjacent vertices
 
 import sys
 sys.setrecursionlimit(10000)
 seen=[]
-dfs(graph,seen,graph[0][0])
+dfs(seen,'1')
 print seen
+
+
+print "The time taken by the algorithm to run"
+print time.time() - start_time, "seconds"
