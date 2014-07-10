6135dbc33fcede29ac8bbf869cd98d21935b107b
Partha Sarathi Mukherjee <partha.s.m.2.010@gmail.com>
2014-07-10 15:09:34 +0530

    Update Randomized_Contraction_algo_list.py
    Introducing advanced file-reading

--- Randomized_Contraction_Algorithm/Randomized_Contraction_algo_list.py
+++ Randomized_Contraction_Algorithm/Randomized_Contraction_algo_list.py
@@ -2,17 +2,10 @@ import time
 start_time = time.time()
 fi = open('kargerMinCut.txt')
 #fi = open('test_case.txt')
-graph=[]
+Graph=[]
 for line in fi:
-    string = line.strip()
-    vertex = int(string[:string.find('\t')])
-    string = string[string.find('\t')+1:]
-    connected_to=[]
-    while string.find('\t')!=-1:
-        connected_to.append(int(string[:string.find('\t')]))
-        string = string[string.find('\t')+1:]
-    connected_to.append(int(string))    
-    graph.append([vertex, connected_to])
+    li = line.split()  
+    Graph.append([li[0], li[1:]])
 #created a list with each row as an item in the list
 #the rows themselves are lists with the 1st element as the vertex
 #the 2nd element of the row is a list containing all the adjacent vertices
@@ -20,31 +13,31 @@ fi.close()
 
 
 
-temp_graph=graph
+TempGraph=Graph
 import random
-while len(temp_graph) > 2:
-    rn = random.randint(0,len(temp_graph)-1)
-    selected_vertex_1 = temp_graph[rn][0]
-    selected_vertex_2 = temp_graph[rn][1][random.randint(0,len(temp_graph[rn][1])-1)]
-    for vc in temp_graph:
+while len(TempGraph) > 2:
+    rn = random.randint(0,len(TempGraph)-1)
+    selected_vertex_1 = TempGraph[rn][0]
+    selected_vertex_2 = TempGraph[rn][1][random.randint(0,len(TempGraph[rn][1])-1)]
+    for vc in TempGraph:
         if vc[0]==selected_vertex_2:
             vc[0]=selected_vertex_1
         vc[1]=[selected_vertex_1 if x==selected_vertex_2 else x for x in vc[1]]
     new_conn=[]
     #Replacing all occurences of selected_vertex_2 with selected_vertex_1
     
-    #print selected_vertex_1, selected_vertex_2, temp_graph
+    #print selected_vertex_1, selected_vertex_2, TempGraph
     #print 'hi'
-    for vc in temp_graph:
+    for vc in TempGraph:
         if vc[0]==selected_vertex_1:
             for v in vc[1]:
                 if v!=selected_vertex_1:
                     new_conn.append(v)
-    temp_graph = [vc for vc in temp_graph if vc[0]!=selected_vertex_1 ]
-    temp_graph.append([selected_vertex_1, new_conn])
+    TempGraph = [vc for vc in TempGraph if vc[0]!=selected_vertex_1 ]
+    TempGraph.append([selected_vertex_1, new_conn])
     #Removed both rows of selcted_vertex_1 AND inserted row with fused vertices
-    #print temp_graph
+    #print TempGraph
      
-print len(temp_graph[0][1])
+print len(TempGraph[0][1])
 print "Time taken for the Randomized Contraction Algorithm to run"
 print time.time() - start_time, "seconds"
