63c78291c32f9357e544d8d3c3b8cb3e78b82a57
Partha Sarathi Mukherjee <partha.s.m.2.010@gmail.com>
2014-07-12 18:57:54 +0530

    Update Djikstra_DD_Unprocessed.py
    Removing the set unprocessed vertices, treating the vertices as numbers.

--- Djikstra/Djikstra_DD_Unprocessed.py
+++ Djikstra/Djikstra_DD_Unprocessed.py
@@ -4,18 +4,17 @@ start_time = time.time()
 fi = open('dijkstraData.txt')
 #fi = open('test_case.txt')
 Graph={}
-UnProcessedVertices = set()
 for line in fi:
     li = line.split()
-    vertex = li[0]
+    v = int(li[0])
     for we in li[1:]:
         we = we.split(',')
-        Graph[vertex] = Graph.get(vertex,{})
-        Graph[we[0]] = Graph.get(we[0],{})
-        Graph[vertex][we[0]] = int(we[1])
-        Graph[we[0]][vertex] = int(we[1])
-        UnProcessedVertices.add(vertex)
-        UnProcessedVertices.add(we[0])
+        w = int(we[0])
+        e = int(we[1])
+        Graph[v] = Graph.get(v,{})
+        Graph[w] = Graph.get(w,{})
+        Graph[v][w] = e
+        Graph[w][v] = e
 #print Graph    
 #created a dictionary with each vertex as the key
 #& the value as an inner dictionary. Each key of this inner dictionary is
@@ -25,23 +24,25 @@ fi.close()
 numVertices = len(Graph)
 
 dict_shortest_path={}
-dict_shortest_path['1']=0
-UnProcessedVertices.remove('1')
+dict_shortest_path[1]=0
 
-while len(UnProcessedVertices) > 0:
+while len(dict_shortest_path) < numVertices:
     minimum = 1000000
-    for w in UnProcessedVertices:
-        di = Graph[w]
-        for v in di:
-            if dict_shortest_path.get(v,-1)!=-1:
-                sp = dict_shortest_path[v]+Graph[v][w]
-                if sp < minimum:
-                    minimum = sp
-                    w_star = w
-    dict_shortest_path[w_star]=minimum                   
-    UnProcessedVertices.remove(w_star)
+    flag=False
+    for w in Graph:
+        if dict_shortest_path.get(w,-1)==-1:
+            flag=True
+            di = Graph[w]
+            for v in di:
+                if dict_shortest_path.get(v,-1)!=-1:
+                    sp = dict_shortest_path[v]+Graph[v][w]
+                    if sp < minimum:
+                        minimum = sp
+                        w_star = w
+    if flag:                        
+        dict_shortest_path[w_star]=minimum                   
 
-print dict_shortest_path['7']
+print dict_shortest_path[7]
 
 print "The time taken by the algorithm to run"
 print time.time() - start_time, "seconds"
