7a173a42fea465066573465179a807df770d1d7a
Partha Sarathi Mukherjee <partha.s.m.2.010@gmail.com>
2014-07-23 20:55:37 +0530

    Create Single_Link_Clustering_Quick_Union.py

---
+++ Union_Find_Applications/Single_Link_Clustering_Quick_Union.py
@@ -0,0 +1,45 @@
+import Quick_Union  
+import time
+start_time = time.time()
+
+fi = open('clustering1.txt')
+EdgeList=[]
+
+
+NumVertices=0
+
+for index,line in enumerate(fi):
+    if index!=0:
+        details = line.split()
+        v = int(details[0])
+        w = int(details[1])
+        EdgeList.append((v,w,int(details[2])))
+    else:
+        NumVertices=int(line.split()[0])
+fi.close()
+
+##Initializing the roots
+Quick_Union.initialize(NumVertices)
+
+
+EdgeList = sorted(EdgeList, key=lambda x:x[2])
+#for e in EdgeList:
+ #   print e
+    
+NumClusters=NumVertices
+for k,e in enumerate(EdgeList):
+    if not Quick_Union.connected(e[0],e[1]):
+        Quick_Union.union(e[0],e[1])
+                
+        NumClusters-=1
+        #print Roots
+        if NumClusters==4:
+            break
+
+for i in range(k+1,len(EdgeList)):            
+    if not Quick_Union.connected(EdgeList[i][0],EdgeList[i][1]):        
+        print EdgeList[i][2]
+        break
+
+print "The time taken by the algorithm to run"
+print time.time() - start_time, "seconds"
