8de0b67cfe7573129d395ebc934e381b98a74162
Partha Sarathi Mukherjee <partha.s.m.2.010@gmail.com>
2014-07-23 23:23:02 +0530

    Create Quick_Union_with_Path_Compression.py

---
+++ Union_Find_Applications/Quick_Union_with_Path_Compression.py
@@ -0,0 +1,22 @@
+Roots=[]
+Roots.append(0)#index of list => vertex; element of list => root
+
+def initialize(n):
+    global Roots
+    Roots*=(n+1)
+    for i in range(n+1):
+        Roots[i]=i
+
+def findRoot(v):
+    while v!=Roots[v]:
+        Roots[v]=Roots[Roots[v]]
+        v=Roots[v]
+    return v        
+
+def union(v,w):
+    rv = findRoot(v)
+    rw = findRoot(w)
+    Roots[rw] = rv
+
+def connected(v,w):
+    return findRoot(v)==findRoot(w)
