861e85ab554dcbde8d85e02fc3a5e84c80997278
Partha Sarathi Mukherjee <partha.s.m.2.010@gmail.com>
2014-07-24 12:54:15 +0530

    Update Kruskal_BFS.py
    Exiting Kruskal's when requisite number of edges added to MST

--- Union_Find_Applications/Kruskal_BFS.py
+++ Union_Find_Applications/Kruskal_BFS.py
@@ -20,26 +20,30 @@ start_time = time.time()
 fi = open('edges.txt')
 EdgeList=[]
 Graph={}
-
+NumVertices=0
 for index,line in enumerate(fi):
     if index!=0:
         details = line.split()
         v = int(details[0])
         w = int(details[1])
         EdgeList.append((v,w,int(details[2])))
-        
+    else:
+        NumVertices= int(line.split()[0])
 fi.close()
 
 EdgeList = sorted(EdgeList, key=lambda x:x[2])
 MinSumEdges=0
+NumEdgesInMST=0
 for e in EdgeList:
     Graph[e[0]]=Graph.get(e[0],[])
     Graph[e[1]]=Graph.get(e[1],[])
     if not bfs(e[0],e[1]):
         Graph[e[0]].append(e[1])
         Graph[e[1]].append(e[0])
+        NumEdgesInMST+=1
         MinSumEdges+=e[2]
-
+        if NumEdgesInMST==(NumVertices-1):
+            break
 print MinSumEdges
 
 print "The time taken by the algorithm to run"
