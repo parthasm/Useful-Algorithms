d00108529eee7ac18489b78dd67f6e03cc2498ab
Partha Sarathi Mukherjee <partha.s.m.2.010@gmail.com>
2014-07-27 23:50:36 +0530

    Update Djikstra_DD_Unprocessed_Heap.py
    Directed Graph now only stores backward edges - i,e, Graph[v][w] is the edge cost of the edge from w to v

--- Single_Source_Shortest_Path/Djikstra_DD_Unprocessed_Heap.py
+++ Single_Source_Shortest_Path/Djikstra_DD_Unprocessed_Heap.py
@@ -11,21 +11,12 @@ for line in fi:
         we = we.split(',')
         w = int(we[0])
         e = int(we[1])
-        Graph[v] = Graph.get(v,{})
-        Graph[v][w] = Graph[v].get(w,[0,0])
-        Graph[v][w][0]=e
-
         Graph[w] = Graph.get(w,{})
-        Graph[w][v] = Graph[w].get(v,[0,0])
-        Graph[w][v][1]=e
+        Graph[w][v]=e
 #print Graph    
-#created a dictionary with each vertex v as the key
+#created a dictionary with each destination vertex v as the key
 #& the value as an inner dictionary. Each key of this inner dictionary is
-# the connected vertex w and the value is a 2-element list.
-#The 1st element of the list is assigned the edge cost
-#if it is a forward edge (from v to w)and
-#the 2nd element is assigned the edge cost
-#if it is a backward edge (from w to v)
+# the source vertex w and the value is the edge cost
 
 fi.close()
 
@@ -46,8 +37,8 @@ while Heap.lengthHeap() > 0 :
     dict_shortest_path[w]=w_score[1]
     di = Graph[w]
     for v in di:
-        if di[v][1]!=0 and dict_shortest_path.get(v,-1)==-1:
-            Heap.modifyKeyIfBetter(v,w_score[1]+di[v][1])
+        if dict_shortest_path.get(v,-1)==-1:
+            Heap.modifyKeyIfBetter(v,w_score[1]+di[v])
 
 
 
